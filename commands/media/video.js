8da751e33580c2294a8ad6dc0aa5d83d5a935283
Repoları oğurlayan peyserdi <117319718+Kolge04@users.noreply.github.com>
2026-06-21 04:26:52 +0400

    Update video.js

--- commands/media/video.js
+++ commands/media/video.js
@@ -15,8 +15,8 @@ module.exports = {
 
   async execute(sock, msg, args) {
     try {
-      // Get instance-specific config
-      const instanceConfig = config.getConfigFromSocket(sock);
+      // Get config directly (Səhv funksiya təmizləndi)
+      const instanceConfig = config;
 
       const text = args.join(' ');
       const chatId = msg.key.remoteJid;
@@ -89,7 +89,7 @@ module.exports = {
         video: { url: videoData.download },
         mimetype: 'video/mp4',
         fileName: `${(videoData.title || videoTitle || 'video').replace(/[^\w\s-]/g, '')}.mp4`,
-        caption: `*${videoData.title || videoTitle || 'Video'}*\n\n> *_Downloaded by ${instanceConfig.botName}_*`
+        caption: `*${videoData.title || videoTitle || 'Video'}*\n\n> *_Downloaded by ${instanceConfig.botName || 'NexusMD ⚡'}_*`
       }, { quoted: msg });
 
     } catch (error) {
