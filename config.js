398bce782ab466808d51df621c02eda58c4fbf0f
Repoları oğurlayan peyserdi <117319718+Kolge04@users.noreply.github.com>
2026-06-22 23:23:40 +0400

    Fix formatting and update names in config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 module.exports = {
     // Bot Owner Configuration
     ownerNumber: ['994775748404'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['NexusMD ⚡'], // Owner names corresponding to ownerNumber array
+    ownerName: ['Nexus MD ⚡'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'NexusMD ⚡',
+    botName: 'Nexus MD ⚡',
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || 'KnightBot!H4sIAAAAAAAAA5VUWbOiOBT+L3nV6ovIIlbdqsEALrgBisvUPCAEiGLAJCDY5X+fwu7b3Q8zPXd4CknqnO98S74CkmOGbNSA4VdQUFwFHLVL3hQIDMGojGNEQRdEAQ/AECQr4eZnDQkQ8hw9gujt7Nnr5i0VsDbrWDwc3TfZSYaumL+DZxcU5SnD4W8KevqIk5p1buexhnbVmMFK3VMo0r7XmOJbaAaqHRHt5NXmO3i2FQNMMUnMIkVXRIPMRs06wPRz8JVJssv03nZlHORYFt26ODLTMMvOw5uLy0u4w7kvK407Jexz8CcNhM780Zv7SWhv4tX2pkV7Lk/dXrbYLJjlufpIqJJ7haVv8BlOCIqmESIc8+bTvA8WNj5gd2Gc+4dOnyuLOjXOxPBJwvqJ/giz8Ujd927HuSV9Dngyt2pYZxkOdgWlXuKUWaW5tgmn65Gx3janvbqJxrdGbqa/Al/TD69c/g/vzjjzH1s4K8rdTWWmvx8L4notqXYxQwvmSXi8M0d7X+Ti4HPw98ItHGwfxnFssHDWb/AYsWARHzxL7TyytXRSe64y2fRz5/ATfsBL+juU+7pKV9BD+yj2/Y09nh/6M7de3Ad+FD9Wj9W5cDankdyMx5NBEezmrNDmoi0+9uOL4ifLw+2hHndn2cKpvCBsqSvF2tGT99dEF9RMIzDsPbuAogQzTgOOc/LaGwy6IIgqD4UU8Re9oImgQC1xFelm3o8HSlFHCld6x8t0hB4MXg7iTU9bpRrhHXRBQfMQMYaiCWY8p80CMRYkiIHhn391AUE1/yZc267f64IYU8a3pCyyPIg+VP04DMIwLwn3GhLCdoEoGAo/txHnmCSs5bEkAQ1TXCGYBpyBYRxkDP2YEFEUgSGnJfqRWphHLfET3YeT+WQDuuD6EgRHYAg0TVJVWZUGkiANRekP9uXelg2K4gtBHHRB9romapImCaraF2RBUduL7f7zB8C2XoR4gDMGhgCuvFC8XpxOQULBORxGUNdtXW9J+xjowxnfmL+qxoFW6ibd2sbV8oxQu8836G7kwszwLWNqY3PmestlBMN/KtJqZ+bpw9pVQoZnR8dq3hIyV7OTyEYqmmpE2VLxLsz1Sc4HAVYmRnmDdb2YrNOSkrdjpSViatH+4oAWknr1rFX/7VTD+3vbLUIVDtGvzbZHWLrWRJF8W5qvCVWz/ExSVy5irwwfac+oiMNgXJd2vVvK12YRmPpsPr0vBqa563jLw1RZr7RlEj3gzr1YZ912K/jds6/MZN/fKvxyUytV+xtj9Io+CVoB/1u6b8BbhwnP7i81vj8m/xLI0bG8i7baESQfJnK6TfrrEAp9pitL6PRcp4F5Crfb82bvEPB8/tUFRRbwOKdXMAQBiWiOI9AFNC9by05JnP+mGdQvUzNJpu3kWcC4/jMGG3xFjAfXAgx76qCnaaIqyF1wbfSi8HjAP9ID9PabOQJ4/g3LxypKWQcAAA==',
@@ -18,7 +18,11 @@ module.exports = {
     MONGODB_URI: "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // Mango db yaz
     
     // Sticker Configuration
-    packname: 'NexusMD ⚡',
+    packname: 'Nexus MD ⚡',
+    
+    // Oyun ucun yer
+    GAME_TIME: '25000', // Oyunda novbeti soze kecid ucin vaxt
+    GAME_NEXT: '4',  // Oyun Sonlanma ucun saygac
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
@@ -75,13 +79,13 @@ module.exports = {
       invalidCommand: '❓ Yanlış Əmr! Kömək Üçün .menu ve ya .help Yazın.'
     },
     
-    // Timezone
+    // Zaman bolgesi
     timezone: 'Asia/Baku',
     
     // Limits
     maxWarnings: 3,
     
-    // Social Links (optional)
+    // Sosiak linkler (optional)
     social: {
       github: 'https://github.com',
       instagram: 'https://instagram.com',
