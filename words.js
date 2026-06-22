a6685b2a4c1e10806664b74b319e1dd12e95af89
Repoları oğurlayan peyserdi <117319718+Kolge04@users.noreply.github.com>
2026-06-22 15:46:08 +0400

    Create words.js

---
+++ words.js
@@ -0,0 +1,7 @@
+module.exports = [
+    { cavab: 'alma', ipucu: 'Ağacda yetişən qırmızı və ya yaşıl meyvə', şablon: 'a*m*' },
+    { cavab: 'banan', ipucu: 'Meyvədir, meymunlar çox sevir', şablon: 'b*n*n' },
+    { cavab: 'İnsan', ipucu: 'Düşünə bilən canlı', şablon: 'i*s*n' },
+    { cavab: 'kompüter', ipucu: 'Kod yazdığınız cihaz', şablon: 'k*m*ü*e*' },
+    { cavab: 'kitab', ipucu: 'Vərəqlərdən ibarət elm mənbəyi', şablon: 'k*t*b' }
+];
