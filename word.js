923daa21e6dd2d01c1c2fa728908f9bb6843d67f
Repoları oğurlayan peyserdi <117319718+Kolge04@users.noreply.github.com>
2026-06-25 14:29:16 +0400

    Add motivational quotes to word.js

---
+++ word.js
@@ -0,0 +1,14 @@
+// commands/fun/words.js
+
+const sozlerBazasi = [
+  "Uğur qazanmaq istəyirsənsə, heç vaxt dayanma! 💪",
+  "Həyat sən planlar qurarkən başına gələnlərdir. 🌀",
+  "Bu gün yeni bir başlanğıc üçün ən gözəl gündür. ✨",
+  "Əgər çətinlik çəkirsənsə, deməli inkişaf edirsən. 📈",
+  "Hər qaranlıq gecənin bir sabahı var, ümidini itirmə. 🌅",
+  "Dahi olmaq üçün deyil, dahi qalmaq üçün çalışmaq lazımdır. 🧠",
+  "Böyük işlər görmək üçün ilk növbədə kiçik addımlara inanmalısan. 👣",
+  "Bu gün etdiyin seçimlər, sabahkı gələcəyini yaradır. 🎯"
+];
+
+module.exports = sozlerBazasi;
